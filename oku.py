346f504a54ec72b1433b75c2ededa29286c5befd
Murat <yuksektepemurat@gmail.com>
2018-04-26 11:45:46 +0300

    OCR oku

---
+++ oku.py
@@ -0,0 +1,15 @@
+import os
+
+import pytesseract
+from PIL import Image
+from flask import Flask
+
+app = Flask(__name__, static_url_path='/static')
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+dosya = Image.open(os.path.join(BASE_DIR, 'static/test.jpg'))
+
+ocr_content = pytesseract.image_to_string(dosya, lang='tur')
+
+print(ocr_content)
